5092bae1bd6e36ed5a72ac96c4f432758bc4f3a5
王俊 <2439456082@qq.com>
2013-07-03 19:06:02 +0800

    User.h更新

--- 源码/PSMS/PSMS/User.h
+++ 源码/PSMS/PSMS/User.h
@@ -1,8 +1,45 @@
 #pragma once
+#include "stdafx.h"
+#include "Record.h"
+#include "TimeRemind.h"
+#include "DateRemind.h"
+#include "Memo.h"
+#include "Diary.h"
+#include "Holiday.h"
+#include "RecordList.h"
 class CUser
 {
-public:
-	CUser(void);
-	~CUser(void);
+		private:
+			 CString m_szName;
+			 CString m_szPassword;
+			 BOOL  isOnline;
+ 
+		public:
+			CUser(void);
+			~CUser(void);
+			CString GetName();
+			CString GetPassword();
+			void  PurgeCDateRemind();
+			void  PurgeDiary();
+			void  PurgeMemo();
+			void  PurgeTimeRemind();
+			CRecord  AddCDateRemind(CDateRemind dateRemind);
+			CRecord  AddDiary(CDiary diary);
+			CRecord  AddMemo(CMemo memo);
+			CRecord  AddTimeRemind(CTimeRemind timeRemind);
+			CRecord  RemoveCDateRemind(CDateRemind CDateRemind);
+			CRecord  RemoveDiary(CDiary diary);
+			CRecord  RemoveMemo(CMemo memo);
+			CRecord  RemoveTimeRemind(CTimeRemind timeRemind);
+			CRecord  UpdateCDateRemind(CDateRemind CDateRemind);
+			CRecord  UpdateDiary(CDiary diary);
+			CRecord  UpdateMemo(CMemo memo);
+			CRecord  UpdateTimeRemind(CTimeRemind timeRemind);
+			CRecordList GetAllCDateRemind();
+			CRecordList GetAllDiary();
+			CRecordList GetAllMemo();
+			CRecordList GetAllTimeRemind();
+
 };
+
 
