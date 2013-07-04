c39d66db3e1b7bf8475e9430bf3117671bcdd98f
yogiman <2292808069@qq.com>
2013-07-04 17:35:15 +0800

    音频调用功能实现
    共五个方法，分别对应五种提醒

--- 源码/PSMS/PSMS/MusicPlayer.cpp
+++ 源码/PSMS/PSMS/MusicPlayer.cpp
@@ -16,17 +16,21 @@ CMusicPlayer::~CMusicPlayer(void)
 //实现成员函数
 void CMusicPlayer::playAudioOfDiary(void)
 {
-	 //PlaySound("..\\PSMS\\800001.wav", NULL, SND_FILENAME | SND_ASYNC);
+	 PlaySound("..\\PSMS\\res\\001.wav", NULL, SND_FILENAME | SND_ASYNC);//使用局部变量
 }
 void CMusicPlayer::playAudioOfMemo(void)
 {
+	PlaySound("..\\PSMS\\res\\002", NULL, SND_FILENAME | SND_ASYNC);//使用局部变量
 }
 void CMusicPlayer::playAudioOfTimeRemind(void)
 {
+	PlaySound("..\\PSMS\\res\\003", NULL, SND_FILENAME | SND_ASYNC);//使用局部变量
 }
 void CMusicPlayer::playAudioOfDateRemind(void)
 {
+	PlaySound("..\\PSMS\\res\\004", NULL, SND_FILENAME | SND_ASYNC);//使用局部变量
 }
 void CMusicPlayer::playAudioOfHoliday(void)
 {
+	PlaySound("..\\PSMS\\res\\005", NULL, SND_FILENAME | SND_ASYNC);//使用局部变量
 }
